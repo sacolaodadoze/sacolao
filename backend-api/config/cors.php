557abaf10e584587eb77e5ae59527f62bf58d599
sacolaodadoze <atendimento@sacolaodadoze.com>
2026-07-28 10:27:38 -0300

    ip a 15.19

--- backend-api/config/cors.php
+++ backend-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' =>  [env('FRONTEND_URL','http://192.168.1.116')],
+    'allowed_origins' =>  [env('FRONTEND_URL','http://192.168.15.19')],
 
     'allowed_origins_patterns' => [],
 
